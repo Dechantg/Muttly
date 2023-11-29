bbc6d73434d8b742c7904d3d2f9da23d2eb01a16
Alexander Da Costa <bsmuse@tutamail.com>
2023-11-29 13:34:18 -0700

    Adjust app.jsx to clear it for our needs, move images to public folder in client and finish home page

--- client/src/components/NavigationBarBottom.jsx
+++ client/src/components/NavigationBarBottom.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 
-export default BottomNavigationBar = () => {
+function BottomNavigationBar() {
   return (
     <div className="bottom-navigation-bar">
-      <Link to="/contactUs" className="nav-link">
+      {/* <Link to="/contactUs" className="nav-link"> */}
         Contact Us
-      </Link>
-      <Link to="/aboutUs" className="nav-link">
+      {/* </Link> */}
+      {/* <Link to="/aboutUs" className="nav-link"> */}
         About Us
-      </Link>
-      <Link to="/signUp" className="nav-link">
+      {/* </Link>
+      <Link to="/signUp" className="nav-link"> */}
         Sign Up
-      </Link>
+      {/* </Link> */}
     </div>
   );
-};
+}; 
+
+export default BottomNavigationBar
