a5ab6d3e27176043067a0f02cf415d81b3a4cfb0
Alexander Da Costa <bsmuse@tutamail.com>
2023-11-29 18:34:05 -0700

    Modify App.js and HomePage to make component more responsive and have navbar bottom to be always on the bottom of the page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,25 +2,27 @@ import './App.css';
 import HomePage from './components/HomePage';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import NavigationBarBottom from './components/NavigationBarBottom';
-import NewsFeedPublicPage from './components/NewsFeedPublicPage';
+// import NewsFeedPublicPage from './components/NewsFeedPublicPage';
 import ContactUsPage from './components/ContactUsPage';
-import AboutUsPage from './components/AboutUsPage';
-import SignUpPage from './components/SignUpPage';
-import SignInModal from './components/SignInModal';
+// import AboutUsPage from './components/AboutUsPage';
+// import SignUpPage from './components/SignUpPage';
+// import SignInModal from './components/SignInModal';
 
 export default function App() {
 
   return (
     <div className="App">
       <BrowserRouter>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/contact" element={<ContactUsPage />} />
-        </Routes>
-        <NavigationBarBottom />
-      </div>
-    </BrowserRouter>
+        <div className="AppWrapper">
+          <div className="AppContent">
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/contact" element={<ContactUsPage />} />
+            </Routes>
+          </div>
+          <NavigationBarBottom />
+        </div>
+      </BrowserRouter>
     </div>
   );
       {/* <HomePage /> */}
@@ -31,4 +33,4 @@ export default function App() {
 // //       <SignInModal />
 //     </div>
 //   );
-// };
+};
