78a5ca4c7cf27fe3f01dcf9103eec6db9a6f2b98
Charles Denahy-Knowles <cjknowles90@gmail.com>
2023-12-01 18:23:22 -0800

    usersFavourite page styling started.

--- client/src/components/UsersFavouritesPage.jsx
+++ client/src/components/UsersFavouritesPage.jsx
@@ -17,21 +17,21 @@ const UsersFavouritesPage = () => {
   };
 
   return (
-    <div classNam="users-favourites-page-container">
+    <div className="users-favourites-page-container">
       {/* Top Navigation Bar */}
       
       {/* Body of the Page */}
       <div className="page-body">
         {/* Header and Subheader */}
         <div className="header">
-          <h2>Your Favourites</h2>
+          <h1>Your Favourites</h1>
           <h3>Check-out the generated images you have favourited...</h3>
         </div>
 
         {/* Grid Layout of Images */}
         <div className="image-grid">
           {/* Render a grid of clickable images */}
-          {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((imageId) => (   // unsure if we need to do it like this; 
+          {[1, 2, 3, 4, ].map((imageId) => (   // unsure if we need to do it like this; 
             <img                                                // can also copy PhotoLabs scss
             key={imageId}
             src={`path/to/image${imageId}.png`}               
@@ -41,15 +41,15 @@ const UsersFavouritesPage = () => {
             ))}
         </div>
 
-        {/* <div className="images"> */}
+        <div className="images">
           {/* Placeholder elements for Images */}
           {/* Alternative for displaying the images? */}
-          {/* <div className="image" onClick={() => handleImageClick(1)}></div> */}
-          {/* <div className="image" onClick={() => handleImageClick(2)}></div> */}
-          {/* <div className="image" onClick={() => handleImageClick(3)}></div> */}
-          {/* <div className="image" onClick={() => handleImageClick(4)}></div> */}
-          {/* <div className="image" onClick={() => handleImageClick(5)}></div> */}
-        {/* </div>  */}
+          <div className="image" onClick={() => handleImageClick(1)}></div> 
+          <div className="image" onClick={() => handleImageClick(2)}></div>
+          <div className="image" onClick={() => handleImageClick(3)}></div>
+          <div className="image" onClick={() => handleImageClick(4)}></div>
+          <div className="image" onClick={() => handleImageClick(5)}></div>
+        </div> 
 
         {/* Dog Breed Card Modal */}
         {selectedImage && <DogBreedCardModal breed={selectedImage} onclick={clearSelectedImage}/>}
