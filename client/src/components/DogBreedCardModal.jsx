219814d232157dcf06ad54b989e6ba1599a7637a
Alexander Da Costa <bsmuse@tutamail.com>
2023-12-02 20:15:50 -0700

    Skeleton and function of DogBreedCardModal is done

--- client/src/components/DogBreedCardModal.jsx
+++ client/src/components/DogBreedCardModal.jsx
@@ -4,36 +4,61 @@ import React, {useEffect, useState } from 'react';
 import Card from './Card';
 import '../views/stylesheets/DogBreedCardModal.scss';
 
-const DogBreedCardModal = () => {
+const DogBreedCardModal = (props) => {
 
   const [liked, setLike] = useState(false) 
+  const [closeModal, setClose] = useState(true)
+  const {image, shedding, drooling, protectiveness, energy, barking, height, weight, name, description, dog1, dog2} = props
 
   const onLikeClick = () => {
     setLike(prev => !prev)
   } 
 
   const onShareClick = () => {
-    alert('Link copied to clipboard!');
+    navigator.clipboard.writeText('pets.ca')
+      .then(() => {
+        console.log('Link copied to clipboard: google.ca');
+        alert('Link copied to clipboard!');
+      })
+      .catch((error) => {
+        console.error('Unable to copy link to clipboard:', error);
+        alert('Failed to copy link to clipboard');
+      });
+  }
+  
 
-  } 
-
-  const onDeleteClick = () => {
+  const onTrashClick = () => {
     alert("I'm trash?! YOU'RE TRASH! GRRRRRRR!")
-    setTimeout(()=> console.log(lol), 1000)
+    setTimeout(()=> setClose(false), 1000)
   }
 
   return (
-    <div className="dog-breed-card">
-      <div className='modal-card-mid-container'>
-        <a><img className='modal-card-icons' onClick={onShareClick} src='../icons/share.png'></img></a>
-        <Card/>
-        <a><img className='modal-card-icons' onClick={onLikeClick} src={
-          liked ? '../icons/heart.png'
-        : '../icons/heart_empty.png'}
-        ></img></a>
-        </div>
-        <a><img className='modal-card-icons' src='../icons/trash-can.png'></img></a>
-    </div>
+    <>
+      {closeModal && <div className="dog-breed-card">
+        <div className='modal-card-mid-container'>
+          <a><img className='modal-card-icons' onClick={onShareClick} src='../icons/share.png'></img></a>
+          <Card
+            image={image || null}
+            shedding={shedding || null}
+            drooling={drooling || null}
+            protectiveness={protectiveness|| null}
+            energy={energy || null}
+            barking={barking || null} 
+            height={height || null} 
+            weight={weight || null}
+            name={name || null} 
+            description={description || null} 
+            dog1 = {dog1}
+            dog2 = {dog2}
+          />
+          <a><img className='modal-card-icons' onClick={onLikeClick} src={
+            liked ? '../icons/heart.png'
+          : '../icons/heart_empty.png'}
+          ></img></a>
+          </div>
+          <a><img className='modal-card-icons'  onClick={onTrashClick} src='../icons/trash-can.png'></img></a>
+      </div>}
+    </>
   );
 };
 
