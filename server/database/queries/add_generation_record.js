f6d6d23480d0c9e65ddf28776290a1c7c3adacd0
Gregory Dechant <dechantg@gmail.com>
2023-12-03 16:40:23 -0800

    fixed error where not all data was being passed

--- server/database/queries/add_generation_record.js
+++ server/database/queries/add_generation_record.js
@@ -6,9 +6,13 @@ const newBreedQuery = (breedQuery) => {
   const { genId, userId, breedOne, breedTwo } = breedQuery;
 
   return db.query (
-    'INSERT INTO new_breed_queries (generated_breeds_id, user_id, breed_one, breed_two) VALUES ($1, $2, $3, $4);',
+    'INSERT INTO new_breed_queries (generated_breeds_id, user_id, breed_one, breed_two) VALUES ($1, $2, $3, $4) RETURNING *;',
     [genId, userId, breedOne, breedTwo]
   )
+  .then(data => {
+    const dogDetails = data.rows;
+    return dogDetails;
+  })
   .catch (error => {
     console.error(`An error has occurred while inserting a new dog like status.`, error);
     throw error;
