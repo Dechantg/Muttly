ff77e577a786f21691a3c5887bfa1cbbc7aac8ef
Gregory Dechant <dechantg@gmail.com>
2023-12-06 12:12:53 -0800

    added extra routes for feting 25 most recent and 25 most liked

--- server/database/queries/most_liked_details.js
+++ server/database/queries/most_liked_details.js
@@ -1,20 +1,27 @@
-
 const db = require('../connection');
 
-
-
 const topLikedDetails = (generatedBreedsIds) => {
-  return db.query(
-    'SELECT * FROM generated_breeds WHERE id IN ($1, $2, $3, $4, $5)', generatedBreedsIds
-    )
-  .then(data => {
-    const topLikedDetails = data.rows;
-    return topLikedDetails;
-  })
-  .catch(error => {
-    console.error('An error occurred while fetching top liked details:', error);
-    throw error;
-  });
+
+
+
+  const placeholders = generatedBreedsIds.map((_, index) => `$${index + 1}`).join(',');
+  
+  
+  
+  const query = `
+    SELECT * FROM generated_breeds WHERE id IN (${placeholders})
+  `;
+
+  return db.query(query, generatedBreedsIds)
+    .then(data => {
+      const topLikedDetails = data.rows;
+      console.log("from in details", topLikedDetails)
+      return topLikedDetails;
+    })
+    .catch(error => {
+      console.error('An error occurred while fetching top liked details:', error);
+      throw error;
+    });
 };
 
 module.exports = topLikedDetails;
